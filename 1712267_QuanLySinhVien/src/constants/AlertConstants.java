a7e799387a3cef63641be21b73a561435c1162ce
linkinben158993 <thienan.nguyenhoang311@gmail.com>
2020-05-29 23:33:49 +0700

    Validate Form And Successfully Add New Students, Problem With New Class If Not Exist

--- 1712267_QuanLySinhVien/src/constants/AlertConstants.java
+++ 1712267_QuanLySinhVien/src/constants/AlertConstants.java
@@ -4,6 +4,14 @@ public class AlertConstants {
 	
 	public static final String NOTFOUND_WARNINGS = "Không tìm thấy người dùng!";
 	public static final String BLANK_FIELD_WARNING = "Vui lòng điền đầy đủ trường!";
+	
+	public static final String BLANK_FIELD_WARNING_NAME = "Vui lòng điền tên Sinh Viên!";
+	public static final String BLANK_FIELD_WARNING_MSSV = "Vui lòng điền mã số Sinh Viên!";
+	public static final String BLANK_FIELD_WARNING_CMND = "Vui lòng điền CMND!";
+	public static final String BLANK_FIELD_WARNING_GENDER ="Vui lòng chọn Nam hoặc Nữ!";
+	public static final String BLANK_FIELD_WARNING_CLASS = "Vui lòng điền lớp!";
+	public static final String BLANK_FIELD_WARNING_CLASS_UNCHECK = "Vui lòng chọn lớp hoặc thêm lớp mới!";
+	
 	public static final String WRONG_FIELD_WARNINGS = "Sai tên đăng nhập hoặc mật khẩu!";
 	public static final String NOTFOUND_BLANK_WRONG_FIELD_PATH = "/resources/images/Alert.gif";
 	
--- 1712267_QuanLySinhVien/src/views/lecturer/LecturerAddStudent.java
+++ 1712267_QuanLySinhVien/src/views/lecturer/LecturerAddStudent.java
@@ -1,21 +1,25 @@
 package views.lecturer;
 
 import java.awt.EventQueue;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import constants.AlertConstants;
 import dao.LopDao;
+import dao.SinhVienDao;
 import entity.Lop;
+import entity.SinhVien;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JRadioButton;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -104,12 +108,13 @@ public class LecturerAddStudent extends JFrame {
 		LopDao lopDao = new LopDao();
 		lops = lopDao.findAll();
 		if (!lops.isEmpty()) {
-			ArrayList<String> listLop = new ArrayList<String>();
-			for (Lop item : lops) {
-				listLop.add(item.get_maLop());
-			}
 
-			list = new JList(listLop.toArray());
+			DefaultListModel<String> listModel = new DefaultListModel<String>();
+			for (Lop item : lops) {
+				listModel.addElement(item.get_maLop());
+			}
+			list = new JList(listModel);
+
 			list.setBounds(120, 200, 150, 150);
 			list.setFont(new Font("Tahoma", Font.BOLD, 11));
 			list.setBackground(Color.LIGHT_GRAY);
@@ -117,9 +122,19 @@ public class LecturerAddStudent extends JFrame {
 		} else {
 			textLop = new JTextField();
 			textLop.setColumns(10);
-			textLop.setBounds(120, 219, 140, 20);
+			textLop.setBounds(120, 220, 140, 20);
 			contentPane.add(textLop);
 		}
+
+		JRadioButton rdbtnNam = new JRadioButton("Nam");
+		rdbtnNam.setBackground(Color.LIGHT_GRAY);
+		rdbtnNam.setBounds(120, 140, 70, 23);
+		contentPane.add(rdbtnNam);
+
+		JRadioButton rdbtnNu = new JRadioButton("Nữ");
+		rdbtnNu.setBackground(Color.LIGHT_GRAY);
+		rdbtnNu.setBounds(192, 140, 70, 23);
+		contentPane.add(rdbtnNu);
 
 		JButton btnNewButton = new JButton("Thêm Mới");
 		btnNewButton.setBackground(Color.LIGHT_GRAY);
@@ -128,26 +143,99 @@ public class LecturerAddStudent extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				if (lops.isEmpty()) {
+
+					String tenSv = textTen.getText();
+					String mssv = textMSSV.getText();
+					String cmnd = textCMND.getText();
 					String lop = textLop.getText();
-					System.out.println(lop);
+					boolean pass = validate_Form(tenSv, mssv, cmnd, lop, rdbtnNam, rdbtnNu);
+					if (pass) {
+
+						SinhVien newSinhVien = new SinhVien();
+						newSinhVien.set_ten(tenSv);
+						newSinhVien.set_mssv(mssv);
+						newSinhVien.set_cmnd(cmnd);
+						newSinhVien.setMa_lop(lop);
+
+						if (rdbtnNam.isSelected()) {
+							newSinhVien.set_gioiTinh(rdbtnNam.getText());
+						} else {
+							newSinhVien.set_gioiTinh(rdbtnNu.getText());
+						}
+
+						newSinhVien.setMa_quyen(2);
+
+						SinhVienDao sinhVienDao = new SinhVienDao();
+						LopDao lopDao = new LopDao();
+						sinhVienDao.insert(newSinhVien);
+						lopDao.insert(new Lop(lop, "CQ" + lop, "Chính Quy"));
+
+						JOptionPane.showMessageDialog(null, "Thêm mới thành công!");
+						dispose();
+
+					} else {
+						JOptionPane.showMessageDialog(null, "Thêm mới thất bại!");
+					}
+
 				} else {
-					String lop = list.getSelectedValue().toString();
-					System.out.println(lop);
+
+					if (list.getSelectedValuesList().size() == 0) {
+
+						JOptionPane.showMessageDialog(null, AlertConstants.BLANK_FIELD_WARNING_CLASS_UNCHECK);
+
+					} else {
+
+						String tenSv = textTen.getText();
+						String mssv = textMSSV.getText();
+						String cmnd = textCMND.getText();
+						String lop = list.getSelectedValue().toString();
+						boolean pass = validate_Form(tenSv, mssv, cmnd, lop, rdbtnNam, rdbtnNu);
+						if (pass) {
+							SinhVien newSinhVien = new SinhVien();
+							newSinhVien.set_ten(tenSv);
+							newSinhVien.set_mssv(mssv);
+							newSinhVien.set_cmnd(cmnd);
+							newSinhVien.setMa_lop(lop);
+
+							if (rdbtnNam.isSelected()) {
+								newSinhVien.set_gioiTinh(rdbtnNam.getText());
+							} else {
+								newSinhVien.set_gioiTinh(rdbtnNu.getText());
+							}
+
+							newSinhVien.setMa_quyen(2);
+
+							SinhVienDao sinhVienDao = new SinhVienDao();
+							sinhVienDao.insert(newSinhVien);
+
+							JOptionPane.showMessageDialog(null, "Thêm mới thành công!");
+							dispose();
+
+						} else {
+							JOptionPane.showMessageDialog(null, "Thêm mới thất bại!");
+						}
+
+					}
 				}
 			}
 		});
-		btnNewButton.setBounds(10, 231, 100, 30);
+		btnNewButton.setBounds(10, 230, 100, 30);
 		contentPane.add(btnNewButton);
 
 		JButton btnLpMi = new JButton("Lớp Mới");
 		btnLpMi.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				// Bỏ JList
 				contentPane.remove(getList());
+				((DefaultListModel) getList().getModel()).clear();
 				contentPane.repaint();
+				lops.removeAll(lops);
+
 				textLop = new JTextField();
 				textLop.setColumns(10);
-				textLop.setBounds(120, 219, 140, 20);
+				textLop.setBackground(Color.LIGHT_GRAY);
+				textLop.setBounds(120, 195, 140, 20);
 				contentPane.add(textLop);
 			}
 		});
@@ -155,10 +243,10 @@ public class LecturerAddStudent extends JFrame {
 		btnLpMi.setBackground(Color.LIGHT_GRAY);
 		btnLpMi.setBounds(10, 313, 100, 30);
 		contentPane.add(btnLpMi);
-		
+
 		JLabel lblLopKhac = new JLabel("Lớp khác:");
 		lblLopKhac.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblLopKhac.setBounds(10, 272, 100, 30);
+		lblLopKhac.setBounds(10, 270, 100, 30);
 		contentPane.add(lblLopKhac);
 	}
 
@@ -174,54 +262,44 @@ public class LecturerAddStudent extends JFrame {
 		JLabel lblNewLabel = new JLabel("Tên Sinh Viên:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel.setBounds(10, 11, 100, 30);
+		lblNewLabel.setBounds(10, 10, 100, 30);
 		contentPane.add(lblNewLabel);
 
 		textTen = new JTextField();
 		textTen.setBackground(Color.LIGHT_GRAY);
-		textTen.setBounds(120, 16, 140, 20);
+		textTen.setBounds(120, 15, 140, 20);
 		contentPane.add(textTen);
 		textTen.setColumns(10);
 
 		JLabel lblMSSinh = new JLabel("Mã số sinh viên:");
 		lblMSSinh.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblMSSinh.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblMSSinh.setBounds(10, 52, 100, 30);
+		lblMSSinh.setBounds(10, 50, 100, 30);
 		contentPane.add(lblMSSinh);
 
 		textMSSV = new JTextField();
 		textMSSV.setBackground(Color.LIGHT_GRAY);
 		textMSSV.setColumns(10);
-		textMSSV.setBounds(120, 57, 140, 20);
+		textMSSV.setBounds(120, 55, 140, 20);
 		contentPane.add(textMSSV);
 
 		JLabel lblCmnd = new JLabel("CMND:");
 		lblCmnd.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblCmnd.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblCmnd.setBounds(10, 93, 100, 30);
+		lblCmnd.setBounds(10, 95, 100, 30);
 		contentPane.add(lblCmnd);
 
 		textCMND = new JTextField();
 		textCMND.setBackground(Color.LIGHT_GRAY);
 		textCMND.setColumns(10);
-		textCMND.setBounds(120, 98, 140, 20);
+		textCMND.setBounds(120, 100, 140, 20);
 		contentPane.add(textCMND);
 
 		JLabel lblGiiTnh = new JLabel("Giới tính:");
 		lblGiiTnh.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblGiiTnh.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblGiiTnh.setBounds(10, 134, 100, 30);
+		lblGiiTnh.setBounds(10, 135, 100, 30);
 		contentPane.add(lblGiiTnh);
-
-		JRadioButton rdbtnNam = new JRadioButton("Nam");
-		rdbtnNam.setBackground(Color.LIGHT_GRAY);
-		rdbtnNam.setBounds(120, 138, 70, 23);
-		contentPane.add(rdbtnNam);
-
-		JRadioButton rdbtnNu = new JRadioButton("Nữ");
-		rdbtnNu.setBackground(Color.LIGHT_GRAY);
-		rdbtnNu.setBounds(192, 138, 70, 23);
-		contentPane.add(rdbtnNu);
 
 		JLabel lblLp = new JLabel("Lớp:");
 		lblLp.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -229,5 +307,32 @@ public class LecturerAddStudent extends JFrame {
 		contentPane.add(lblLp);
 
 		event_listener();
+	}
+
+	private boolean validate_Form(String tenSv, String mssv, String cmnd, String lop, JRadioButton rdbtnNam,
+			JRadioButton rdbtnNu) {
+		try {
+			if (tenSv.isEmpty()) {
+				JOptionPane.showMessageDialog(null, AlertConstants.BLANK_FIELD_WARNING_NAME);
+				return false;
+			} else if (mssv.isEmpty()) {
+				JOptionPane.showMessageDialog(null, AlertConstants.BLANK_FIELD_WARNING_MSSV);
+				return false;
+			} else if (cmnd.isEmpty()) {
+				JOptionPane.showMessageDialog(null, AlertConstants.BLANK_FIELD_WARNING_CMND);
+				return false;
+			} else if (!rdbtnNam.isSelected() && !rdbtnNu.isSelected()) {
+				JOptionPane.showMessageDialog(null, AlertConstants.BLANK_FIELD_WARNING_GENDER);
+				return false;
+			} else if (lop.isEmpty()) {
+				JOptionPane.showMessageDialog(null, AlertConstants.BLANK_FIELD_WARNING_CLASS);
+				return false;
+			} else {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
